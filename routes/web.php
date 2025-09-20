2a031a0868cb9b0f229d9cd96fb53329720ed19a
Er. Reda <redaermich@gmail.com>
2025-09-20 18:45:35 +0100

    feat: restructure shipment and order routes for improved organization and clarity

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,27 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('Dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::apiResource('suppliers', SupplierController::class)->except('show');
+
     Route::apiResource('customers', CustomerController::class)->except('show');
+
     Route::apiResource('products', ProductController::class)->except('show');
+
     Route::apiResource('users', UserController::class)->except('show');
-    Route::get('shipments/export-data', [ShipmentController::class, 'exportData'])->name('shipments.exportData');
+
     Route::apiResource('shipments', ShipmentController::class);
-    Route::get('shipments/{shipment}', [ShipmentController::class, 'show'])->name('shipments.show');
-    Route::post('shipments/{shipment}/customers', [OrderController::class, 'createCustomerAndAttachOrder'])->name('shipments.customers.attachCreate');
-    Route::post('shipments/{shipment}/customers/{customer}/attach-order', [OrderController::class, 'attachOrderToShipment'])->name('shipments.customers.attachOrder');
+    Route::prefix('shipments')->name('shipments.')->group(function () {
+        Route::get('export-data', [ShipmentController::class, 'exportData'])->name('exportData');
+        Route::get('{shipment}', [ShipmentController::class, 'show'])->name('show');
+        Route::post('{shipment}/customers', [OrderController::class, 'createCustomerAndAttachOrder'])->name('customers.attachCreate');
+        Route::post('{shipment}/customers/{customer}/attach-order', [OrderController::class, 'attachOrderToShipment'])->name('customers.attachOrder');
+    });
+
     Route::apiResource('orders', OrderController::class);
-    Route::post('orders/{order}/attach-product', [OrderController::class, 'attachProduct'])->name('orders.attachProduct');
+    Route::prefix('orders')->name('orders.')->group(function () {
+        Route::post('{order}/attach-product', [OrderController::class, 'attachProduct'])->name('attachProduct');
+    });
 });
 
 require __DIR__ . '/settings.php';
